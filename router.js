c4da8f18057113a43283224a6830d5c93e150df1
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-04 15:08:06 -0300

    fix: requisito 1

--- router.js
+++ router.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const data = require('./data');
+const fs = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND_STATUS = 404;
 
 const router = express.Router();
 
-router.get('/talker', (request, response) => {
-  if (data.length !== 0) return response.status(HTTP_OK_STATUS).json(data);
-  response.status(HTTP_OK_STATUS).json([]);
+const getData = async (request, _response, next) => {
+  try {
+    const data = await fs.readFile('./talker.json');
+    request.data = JSON.parse(data);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+router.get('/talker', getData, (request, response) => {
+  const { data } = request;
+   response.status(HTTP_OK_STATUS).json(data);
 });
 
-router.get('/talker/:id', (request, response) => {
+router.get('/talker/:id', getData, (request, response) => {
   const { id } = request.params;
+  const { data } = request;
+
   const talkerId = data.find((elem) => elem.id === parseInt(id, 10));
   if (!talkerId) {
  return response.status(HTTP_NOT_FOUND_STATUS)
@@ -20,5 +32,4 @@ router.get('/talker/:id', (request, response) => {
 }
 return response.status(HTTP_OK_STATUS).json(talkerId);
 });
-
 module.exports = router; 
