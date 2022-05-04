115b29897ecc48e29d6197abec8164aa144b7ecb
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-04 15:06:08 -0300

    feat: requisito 2

--- router.js
+++ router.js
@@ -2,12 +2,23 @@ const express = require('express');
 const data = require('./data');
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 
 const router = express.Router();
 
 router.get('/talker', (request, response) => {
   if (data.length !== 0) return response.status(HTTP_OK_STATUS).json(data);
   response.status(HTTP_OK_STATUS).json([]);
+});
+
+router.get('/talker/:id', (request, response) => {
+  const { id } = request.params;
+  const talkerId = data.find((elem) => elem.id === parseInt(id, 10));
+  if (!talkerId) {
+ return response.status(HTTP_NOT_FOUND_STATUS)
+  .json({ message: 'Pessoa palestrante não encontrada' }); 
+}
+return response.status(HTTP_OK_STATUS).json(talkerId);
 });
 
 module.exports = router; 
